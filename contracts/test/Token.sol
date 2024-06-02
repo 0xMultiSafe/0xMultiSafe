6bf884af18c8b71ec042951aa17cc1c77ba9753c
ahmedhamedaly <ahmedhamedaly19@gmail.com>
2024-06-02 03:40:39 +0200

    chore: prep for demoing

--- contracts/test/Token.sol
+++ contracts/test/Token.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.24;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
-    constructor() ERC20("Test Token", "TEST") {
+    constructor() ERC20("USD Coin", "USDC") {
         _mint(msg.sender, 100_000_000 ether);
     }
 }
